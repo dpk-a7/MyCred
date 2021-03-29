e15756345e6fa992f5c7bd53f5e04bcd751a13e4
Deepak Avudiappan <55245100+dpk-a7@users.noreply.github.com>
2021-03-29 21:57:28 +0530

    Update keyGenerator.py

--- keyGenerator.py
+++ keyGenerator.py
@@ -1,10 +1,7 @@
-#import the module
 from cryptography.fernet import Fernet
 
-#generate a key and store it as key object
 key = Fernet.generate_key()
-
-#save the key to the file 
+ 
 with open('do_not_delete.key', 'wb') as filekey:
 	filekey.write(key)
 print("Key generated successfully\nName: do_not_delete.key\nPlease do not delete this file after encrypting the folder!")
