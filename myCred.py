489c3a30a73e8fe9a60703d99ff2b4c9f6392cdf
Deepak Avudiappan <55245100+dpk-a7@users.noreply.github.com>
2021-05-04 16:40:46 +0530

    updated with terminal animation

--- myCred.py
+++ myCred.py
@@ -1,38 +1,40 @@
 import os
-import argparse
 import sys
-from cryptography.fernet import Fernet
+import pip
+import platform
 
-with open('do_not_delete.key') as filekey:
-	key = filekey.read()
-_fernet = Fernet(key)
+os_type = platform.uname()
+opsys = os_type[0]
+opnm = os_type[1]
+try:
+	import argparse
+	from cryptography.fernet import Fernet
+	from progress.bar import IncrementalBar
+except:
+	if hasattr(pip, 'main'):
+		pip.main(['install',"argparse"])
+		pip.main(['install',"cryptography"])
+		pip.main(['install', 'progress'])
+	else:
+		pip._internal.main(['install','argparse'])
+		pip._internal.main(['install','cryptography'])
+		pip._internal.main(['install', 'progress'])
+finally:
+	import argparse
+	from cryptography.fernet import Fernet
+	from progress.bar import IncrementalBar
+try:
+	with open('do_not_delete.key') as filekey:
+		key = filekey.read()
+	_fernet = Fernet(key)
+except:
+	print("do_not_delete.key not found!")
 
-def encryptor_(foldername):
-	all_file = []
-	os.chdir(f'./{foldername}')
-	for i in os.listdir():
-		all_file.append(str(i))
-	for j in all_file:
-		with open(j, 'rb') as file:
-			original = file.read()
-		encrypted = _fernet.encrypt(original)
-		with open(j, 'wb') as encrfile:
-			encrfile.write(encrypted)
-	os.chdir(f'..')
-	print("Encryption done successfully!")
-def decryptor_(foldername):
-	all_file = []
-	os.chdir(f'./{foldername}')
-	for i in os.listdir():
-		all_file.append(str(i))
-	for j in all_file:
-		with open(j, 'rb') as file:
-			original = file.read()
-		encrypted = _fernet.decrypt(original)
-		with open(j, 'wb') as encrfile:
-			encrfile.write(encrypted)
-	print("Decryption done successfully!")
-
+def Generator():
+	key = Fernet.generate_key()
+	with open('do_not_delete.key', 'wb') as filekey:
+		filekey.write(key)
+	print(f"\nKey generated: do_not_delete.key \nPath: {os.getcwd()}")
 
 def fld(name : str)-> "Name of the folder":
 	if name.m:
@@ -56,26 +58,104 @@ def fld(name : str)-> "Name of the folder":
 				print(f"{name.r} not found!")
 		except Exception:
 			print("Error Occured while executing the command!")
+	elif name.g == 'y':
+		Generator()
 	elif name.hd:
 		try:
 			if name.hd in os.listdir():
-				encryptor_(name.hd)
-				os.system(f"Attrib +h +s +r {name.hd}")
-				print(f"{name.hd} hide successful!")
+				unchn = []
+				bar_ = IncrementalBar(max = len(os.listdir(name.hd)))
+				for i in os.listdir(name.hd):
+					if os.path.isdir(name.hd+"/"+ i) == True:
+						foldername = name.hd+"/"+ i
+						os.chdir(f'./{foldername}')
+						all_file = os.listdir()
+						bar = IncrementalBar(max = len(all_file))
+						for i__ in all_file:
+							with open(i__, 'rb') as file:
+								original = file.read()
+							encrypted = _fernet.encrypt(original)
+							with open(i__, 'wb') as encrfile:
+								encrfile.write(encrypted)
+							bar.next()
+						for _ in range(2):
+							os.chdir('..')
+
+					elif os.path.isdir(name.hd+"/"+ i) == False:
+						file_name = name.hd+"/"+ i
+						with open(file_name, 'rb') as file:
+							original = file.read()
+						encrypted = _fernet.encrypt(original)
+						with open(file_name, 'wb') as encrfile:
+							encrfile.write(encrypted)
+					else:
+						unchn.append(i)
+					bar_.next()
+				if opsys == "Windows":
+					os.system(f"Attrib +h +s +r {name.hd}")
+				elif opsys == "Linux":
+					os.system(f"mv {name.hd} .{name.hd}")
+				print("\n-->Encryption done successfully!")
+				print(f"-->Dir: {name.hd} hide successful!")
+				X = "File that were unchanged!" if len(unchn) != 0 else ""
+				print(X)
+				if len(unchn) != 0:
+					for i in unchn:
+						print(i)
 			else:
 				print(f"{name.hd} not found!")
-		except Exception:
-			print("Error Occured while executing the command!")
+		except Exception as e:
+			print(f" Error Occured while executing the command!\n\
+				* Maybe the {name.hd} is already hidden 'OR'\n\
+				* The Files are not Encriptable!\n<Error Message>\n",e)
 	elif name.uh:
 		try:
 			if name.uh in os.listdir():
-				os.system(f"Attrib -h -s -r {name.uh}")
-				decryptor_(name.uh)
-				print(f"{name.uh} unhide successful!")
+				unchn = []
+				bar_ = IncrementalBar( max = len(os.listdir(name.uh)))
+				for i in os.listdir(name.uh):
+					if os.path.isdir(name.uh+"/"+ i) == True:
+						foldername = name.uh+"/"+ i
+						os.chdir(f'./{foldername}')
+						all_file = os.listdir()
+						bar = IncrementalBar( max = len(all_file))
+						for i__ in all_file:
+							with open(i__, 'rb') as file:
+								original = file.read()
+							decrypted = _fernet.decrypt(original)
+							with open(i__, 'wb') as decrfile:
+								decrfile.write(decrypted)
+							bar.next()
+						for _ in range(2):
+							os.chdir('..')
+
+					elif os.path.isdir(name.uh+"/"+ i) == False:
+						file_name = name.uh+"/"+ i
+						with open(file_name, 'rb') as file:
+							original = file.read()
+						decrypted = _fernet.decrypt(original)
+						with open(file_name, 'wb') as decrfile:
+							decrfile.write(decrypted)
+					else:
+						unchn.append(i)
+					bar_.next()
+				if opsys == "Windows":
+					os.system(f"Attrib -h -s -r {name.uh}")
+				elif opsys == "Linux":
+					os.system(f"mv .{name.uh} {name.uh}")
+				print("\n-->Decryption done successfully!")
+				print(f"-->Dir: {name.uh} unhide successful!")
+				X = "File that were unchanged!" if len(unchn) != 0 else ""
+				print(X)
+				if len(unchn) != 0:
+					for i in unchn:
+						print(i)
 			else:
 				print(f"{name.uh} not found!")
-		except Exception:
-			print("Error Occured while executing the command")
+		except Exception as e:
+			print(f" Error Occured while executing the command!\n\
+				* Maybe the {name.uh} is already visible 'OR'\n\
+				* The Files are not Decriptable!\n<Error Message>\n",e)
 	elif name.ls == "this":
 		for i in os.listdir():
 			print(i)
@@ -90,10 +170,11 @@ def fld(name : str)-> "Name of the folder":
 			print(f"{name.ls} not found!")
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
-	parser.add_argument('-m', type = str,help = "Enter name to make folder")
-	parser.add_argument('-r', type = str,help = "Enter name to remove folder")
-	parser.add_argument('-hd', type = str,help = "Enter name to hide folder")
-	parser.add_argument('-uh', type = str,help = "Enter name to unhide folder")
-	parser.add_argument('-ls', type = str, help = "List all the items ")
+	parser.add_argument('-g', type = str,help = "Generate 'New Key'---------->[myCred.py -g y]")
+	parser.add_argument('-m', type = str,help = "Enter name to make folder--->[myCred.py -m <dir name>]")
+	parser.add_argument('-r', type = str,help = "Enter name to remove folder->[myCred.py -r <dir name>]")
+	parser.add_argument('-hd', type = str,help = "Enter name to hide folder--->[myCred.py -hd <dir name>]")
+	parser.add_argument('-uh', type = str,help = "Enter name to unhide folder->[myCred.py -uh <dir name>]")
+	parser.add_argument('-ls', type = str, help = "List all the items---------->[myCred.py -ls this] or [myCred.py -ls <dir name>]")
 	arg = parser.parse_args()
 	fld(arg)
